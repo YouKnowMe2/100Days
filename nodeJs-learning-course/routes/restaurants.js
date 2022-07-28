d47b06a8d48a09f1d33a9759acbbd04261252571
youknowme2 <highermath80@gmail.com>
2022-07-28 14:57:20 +0600

    [NodeJsLearning] Query Parameter Implemented for sorting

--- nodeJs-learning-course/routes/restaurants.js
+++ nodeJs-learning-course/routes/restaurants.js
@@ -2,16 +2,33 @@ const express = require('express');
 const router = express.Router();
 
 const fs = require('fs');
-
 const path = require ('path');
 const uuid = require('uuid');
 const resData = require('../utili/restaurant-data');
 
 router.get('/restaurants',function (req, res,){
-    
+    const order = req.query.order;
+    let nextOrder = 'desc';
     const storeRestaurants = resData.getStoredRestaurant();
+
+  
+    if(order != 'asc' && order != 'desc'){
+        order = 'asc';
+    }
+    if(order === 'desc'){
+        nextOrder = 'asc';
+    }
+    storeRestaurants.sort(function(resA,resB){
+        if( (order === 'asc' && resA.name > resB.name) || (order === 'desc' && resB.name > resA.name) )
+        {
+            return 1
+        }
+        return -1
+    });
+
     res.render('restaurants',{
-        restaurants: storeRestaurants
+        restaurants: storeRestaurants,
+        nextOrder: nextOrder
     });
 });
 
