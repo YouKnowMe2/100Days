36347519870cb5ee8a14a3645f1c384be6136775
youknowme2 <highermath80@gmail.com>
2022-07-30 16:16:41 +0600

    [Advanced Javascript] Asynchoronus Task with promises

--- advanced-javascript/async.js
+++ advanced-javascript/async.js
@@ -1,13 +1,25 @@
-const fs = require('fs');
+const fs = require('fs/promises');
 
 function readFile(){
     let fileData;
-    fs.readFile('data.txt',function (error,fileData){
+    // fs.readFile('data.txt',function (error,fileData){
+    //
+    //     console.log('File Parsing done.');
+    //     console.log(fileData.toString());
+    //
+    //     //start another async task that sends teh data
+    //
+    // });
+    fs.readFile('data.txt').then(function (fileData){
+            console.log('File Parsing done.');
+            console.log(fileData.toString());
+            //return anotherASyncOperation
+    }).then( function () {
 
-        console.log('File Parsing done.');
-        console.log(fileData.toString());
+    }).catch(function (error){
+        console.log(error);
     });
-
+    //promised have then method
     console.log('Completed the works.');
 
 }
