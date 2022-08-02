c7f073b8737a34b78bc237014c5cb3547dc7a927
youknowme2 <highermath80@gmail.com>
2022-08-02 23:49:03 +0600

    [database-nodejs]Fetched data and showed single

--- database-nodejs/routes/blog.js
+++ database-nodejs/routes/blog.js
@@ -4,11 +4,15 @@ const router = express.Router();
 const db = require('../data/database');
 
 router.get('/',function (req,res){
+
     res.redirect('/posts');
 });
 
-router.get('/posts',function (req,res){
-    res.render('posts-list');
+router.get('/posts', async function (req,res){
+    const[posts] = await db.query('SELECT posts.*, authors.name AS author_name FROM posts INNER JOIN authors ON posts.author_id = authors.id');
+    res.render('posts-list',{
+        posts: posts
+    });
 });
 router.get('/new-post',async function (req,res){
     const [authors] = await db.query('SELECT * FROM authors');
@@ -27,6 +31,17 @@ router.post('/store-data',async function (req, res){
     await db.query('INSERT INTO posts (title, summary, body, author_id)VALUES (?)',[data] );
 
    res.redirect('/posts');
+});
+
+router.get('/post-details/:id',async function (req,res){
+    const postId = req.params.id;
+    const [posts] = await db.query('SELECT posts.*, authors.name AS author_name, authors.email AS author_email FROM posts INNER JOIN authors ON posts.author_id = authors.id WHERE posts.id = ?',[postId]);
+    if( !posts || posts.length ==0){
+        return res.status(404).render('404');
+    }
+    res.render('post-detail',{
+        post: posts[0]
+    });
 });
 
 
