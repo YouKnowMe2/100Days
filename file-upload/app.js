34af6c94533718ef6bfacbf3e285e79dcb0bf666
youknowme2 <highermath80@gmail.com>
2022-08-05 11:19:39 +0600

    [file-upload] Fetching all info and showing

--- file-upload/app.js
+++ file-upload/app.js
@@ -11,7 +11,10 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: false }));
+
 app.use(express.static('public'));
+
+app.use('/images',express.static('images'));
 
 app.use(userRoutes);
 
--- file-upload/routes/users.js
+++ file-upload/routes/users.js
@@ -4,7 +4,7 @@ const multer = require('multer');
 const db = require('../data/database')
 const storageConfigh = multer.diskStorage({
     destination: function (req,res,cb){
-        cb(null, 'storage/images');
+        cb(null, 'images');
     },
     filename: function (req,file,cb){
         cb(null,Date.now()+ '-' + file.originalname);
@@ -15,8 +15,11 @@ const upload = multer({ storage: storageConfigh });
 const router = express.Router();
 
 
-router.get('/', function(req, res) {
-  res.render('profiles');
+router.get('/', async function(req, res) {
+    const users = await db.getDb().collection('users').find().toArray();
+  res.render('profiles',{
+      users: users
+  });
 });
 
 router.get('/new-user', function(req, res) {
