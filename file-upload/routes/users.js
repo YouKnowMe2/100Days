46f2a49f8e0344e4e972ce29df3cf042f2c556a0
youknowme2 <highermath80@gmail.com>
2022-08-05 10:19:38 +0600

    [file-upload] Image upload

--- file-upload/routes/users.js
+++ file-upload/routes/users.js
@@ -1,4 +1,15 @@
 const express = require('express');
+const multer = require('multer');
+
+const storageConfigh = multer.diskStorage({
+    destination: function (req,res,cb){
+        cb(null, 'storage/images');
+    },
+    filename: function (req,file,cb){
+        cb(null,Date.now()+ '-' + file.originalname);
+    }
+})
+const upload = multer({ storage: storageConfigh });
 
 const router = express.Router();
 
@@ -8,6 +19,14 @@ router.get('/', function(req, res) {
 
 router.get('/new-user', function(req, res) {
   res.render('new-user');
+});
+router.post('/profiles',upload.single('image'),function (req,res){
+ const uploadImageFile = req.file;
+
+ const userData = req.body;
+ console.log(uploadImageFile);
+ console.log(userData);
+ res.redirect('/');
 });
 
 module.exports = router;
