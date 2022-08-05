799909bf12a9748bbe9e35c805c188eaed5fe574
youknowme2 <highermath80@gmail.com>
2022-08-05 12:47:05 +0600

    [ajax] Comments loaded without page reloading

--- ajax/public/scripts/comments.js
+++ ajax/public/scripts/comments.js
@@ -1,10 +1,36 @@
 const loadComments = document.getElementById('load-comments');
+const commentSection = document.getElementById('comments');
+
+function createCommentsList(comments){
+
+    const commentListElement = document.createElement('ol');
+
+    for(const comment of comments){
+        const commentElement = document.createElement('li');
+
+        commentElement.innerHTML = `
+            <article class="comment-item">
+                <h2> ${comment.title}</h2>
+                <p> ${comment.text} </p>
+            </article>`
+        commentListElement.appendChild(commentElement);
+    }
+    return commentListElement;
+}
+
 
 async function fetchComments(event){
     const postId = loadComments.dataset.postid;
     const response = await fetch(`/posts/${postId}/comments `);
     const data = await response.json();
-    console.log(data);
+
+    const responseData = createCommentsList(data);
+
+    commentSection.innerHTML= '';
+    commentSection.appendChild(responseData);
+
 }
+
+
 
 loadComments.addEventListener('click',fetchComments);
