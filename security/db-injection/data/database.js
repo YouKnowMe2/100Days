5a64907d9c68a4035b2d1c14ac462a288d91855d
youknowme2 <highermath80@gmail.com>
2022-08-08 12:28:00 +0600

    [security] Secuirty added for sql-injection

--- security/db-injection/data/database.js
+++ security/db-injection/data/database.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  uri: 'localhost',
+  uri: 'localhost:8000',
   database: 'security',
   user: 'root',
-  password: 'your-pw',
-  multipleStatements: true
+  password: '',
+  // multipleStatements: true (stopped for protection)
 })
 
 module.exports = pool;
+
