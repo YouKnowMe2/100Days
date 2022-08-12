9320c4df2699a006faa77a137fcb78a3f498ae7f
youknowme2 <highermath80@gmail.com>
2022-08-12 13:17:24 +0600

    [mvc] Created Models

---
+++ mvc/models/post.js
@@ -0,0 +1,20 @@
+const db = require("../data/database");
+
+class Post {
+    constructor(title,content,id) {
+       this.title = title;
+       this.content = content;
+       this.id = id;
+
+    }
+   async insert(){
+
+        await db.getDb().collection('posts').insertOne({
+            title: this.title,
+            content: this.content,
+        });
+    }
+
+}
+
+module.exports = Post;
