a9fbe4506312a6d653f868e3b7de92e9bbe0d143
youknowme2 <highermath80@gmail.com>
2022-08-26 16:37:14 +0600

    [OnlineShop] Showing Product in the Site

--- online-shop/models/product.model.js
+++ online-shop/models/product.model.js
@@ -9,8 +9,18 @@ class Product{
         this.image = productData.image;
         this.imagePath = `product-data/images/${productData.image}`;
         this.imageUrl = `/products/assets/images/${productData.image}`;
-    }
+        if(productData._id){
+            this.id = productData._id.toString();
+        }
 
+    }
+   static async findAll(){
+       const products = await db.getDb().collection('products').find().toArray();
+
+       return products.map(function (product){
+            return new Product(product);
+       });
+   }
    async save(){
         const productData = {
             title: this.title,
