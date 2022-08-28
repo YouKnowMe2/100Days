0152a40f736685464b14ff795fc36d3dfc0cfa7a
youknowme2 <highermath80@gmail.com>
2022-08-28 20:00:44 +0600

    [OnlineShop] Added Product Update Method

--- online-shop/controllers/admin.controller.js
+++ online-shop/controllers/admin.controller.js
@@ -1,31 +1,57 @@
 const Product = require('../models/product.model');
 
-async function getProducts(req,res,next){
-    try{
+async function getProducts(req, res, next) {
+    try {
         const products = await Product.findAll();
-        res.render('admin/products/all-products',{
-            products: products
-        });
-    }catch (error){
+        res.render('admin/products/all-products', { products: products });
+    } catch (error) {
+        next(error);
+        return;
+    }
+}
+
+function getNewProduct(req, res) {
+    res.render('admin/products/new-product');
+}
+
+async function createNewProduct(req, res, next) {
+    const product = new Product({
+        ...req.body,
+        image: req.file.filename,
+    });
+
+    try {
+        await product.save();
+    } catch (error) {
         next(error);
         return;
     }
 
+    res.redirect('/admin/products');
 }
 
-function getNewProduct(req,res){
-    res.render('admin/products/new-product');
+async function getUpdateProduct(req, res, next) {
+    try {
+        const product = await Product.findById(req.params.id);
+        res.render('admin/products/update-product', { product: product });
+    } catch (error) {
+        next(error);
+    }
 }
 
-async function createNewProduct(req,res,next){
+async function updateProduct(req, res, next) {
     const product = new Product({
         ...req.body,
-        image: req.file.filename
+        _id: req.params.id,
     });
-    try{
-        await product.save()
 
-    }catch (error){
+    if (req.file) {
+        product.replaceImage(req.file.filename);
+    }
+
+    try {
+        await product.save();
+    } catch (error) {
         next(error);
         return;
     }
@@ -36,5 +62,7 @@ async function createNewProduct(req,res,next){
 module.exports = {
     getProducts: getProducts,
     getNewProduct: getNewProduct,
-    createNewProduct : createNewProduct,
-}
+    createNewProduct: createNewProduct,
+    getUpdateProduct: getUpdateProduct,
+    updateProduct: updateProduct,
+};
