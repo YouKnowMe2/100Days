82cf769e090190479d35650e974004b97a3097f7
youknowme2 <highermath80@gmail.com>
2022-08-29 10:48:01 +0600

    [OnlineShop] Deleting Data

---
+++ online-shop/public/scripts/product-management.js
@@ -0,0 +1,21 @@
+const btnELement = document.querySelectorAll('.product-item button');
+
+async function deleteProduct(event){
+    const buttonElement = event.target;
+    const productId = buttonElement.dataset.productid;
+    const csrfToken = buttonElement.dataset.csrf;
+
+    const response = await fetch('/admin/products/' + productId + '?_csrf='+ csrfToken,{
+        method: 'DELETE',
+    });
+
+    if(!response.ok){
+        alert('Something Went Wrong');
+        return;
+    }
+    buttonElement.parentElement.parentElement.parentElement.parentElement.remove();
+}
+
+for(const deleteProductButtonElement of btnELement){
+    deleteProductButtonElement.addEventListener('click',deleteProduct);
+}
