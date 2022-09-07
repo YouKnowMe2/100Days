316bd58a581d89e0f23ea957b1944f968b8c3cde
youknowme2 <highermath80@gmail.com>
2022-09-07 16:35:20 +0600

    END OF COURSE--THANK YOU

--- 06-adding-a-decoupled-frontend/scripts/app.js
+++ 06-adding-a-decoupled-frontend/scripts/app.js
@@ -1,6 +1,7 @@
 const TodosApp = {
     data(){
         return {
+            isLoading: false,
            todos: [],
             enteredTodoText: '',
             editedTodoId: null,
@@ -9,13 +10,15 @@ const TodosApp = {
     },
     methods:{
 
-        saveTodo(event){
+       async saveTodo(event){
             event.preventDefault();
             if(this.editedTodoId){
+
                 const todoId = this.editedTodoId;
                 const todoIndex = this.todos.findIndex(function(todoItem){
                     return todoItem.id === todoId;
                 });
+
                 const updatedTodoItem = {
                     id: this.todos[todoIndex].id,
                     text:  this.enteredTodoText
@@ -23,11 +26,62 @@ const TodosApp = {
                 this.todos[todoIndex] = updatedTodoItem;
                 this.editedTodoId = null;
 
+                let response;
+
+                    try {
+                        response = await fetch('http://localhost:3000/todos/' + todoId, {
+                        method: 'PATCH',
+                        body: JSON.stringify({
+                            text: this.enteredTodoText,
+                        }),
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        });
+                    } catch (error) {
+                        alert('Something went wrong!');
+                        return;
+                    }
+
+                    if (!response.ok) {
+                        alert('Something went wrong!');
+                        return;
+                    }
+
+           
+
             }else{
+                //Creating
+                let response;
+
+                try {
+                    response = await fetch('http://localhost:3000/todos', {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        text: this.enteredTodoText,
+                    }),
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    });
+                } catch (error) {
+                    alert('Something went wrong!');
+                    return;
+                }
+
+                if (!response.ok) {
+                    alert('Something went wrong!');
+                    return;
+                }
+
+                const responseData = await response.json();
+                const todoId = responseData.createdTodo.id;
                 const newTodo = {
                     text: this.enteredTodoText,
-                    id: new Date().toISOString()
+                    id: todoId
                 };
+                
+                
                 this.todos.push(newTodo);
                 
             }
@@ -37,18 +91,54 @@ const TodosApp = {
         startEditTodo(todoId){
             this.editedTodoId = todoId;
             const todo = this.todos.find(function(todoItem){
-                return todoItem.id !== todoId;
+                return todoItem.id === todoId;
             });
             this.enteredTodoText = todo.text;
 
         },
-        deleteTodo(todoId){
+        async deleteTodo(todoId){
             this.todos = this.todos.filter(function(todoItem){
                 return todoItem.id !== todoId;
             });
+            let response;
+
+            try {
+                response = await fetch('http://localhost:3000/todos/' + todoId, {
+                method: 'DELETE',
+                });
+            } catch (error) {
+                alert('Something went wrong!');
+                return;
+            }
+
+            if (!response.ok) {
+                alert('Something went wrong!');
+                return;
+            }
             
         },
-    }
+    },
+    async created(){
+        let response;
+        this.isLoading = true;
+        try {
+            response = await fetch('http://localhost:3000/todos');
+        } catch (error) {
+            alert('Something went wrong!');
+            this.isLoading = false;
+            return;
+        }
+        this.isLoading = false;
+
+        if (!response.ok) {
+            alert('Something went wrong!');
+            return;
+        }
+
+        const responseData = await response.json();
+        this.todos = responseData.todos;
+
+        }
 };
 
 Vue.createApp(TodosApp).mount('#todos-app');        
